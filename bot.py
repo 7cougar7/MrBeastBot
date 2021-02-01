be8956f0c68f433733442edbe4839eabdaec2367
7cougar7 <tilogarcia1@gmail.com>
2021-02-01 14:49:08 -0600

    Bug Fix

--- bot.py
+++ bot.py
@@ -10,9 +10,13 @@ MrBeast = commands.Bot(command_prefix='~~~~')
 
 @MrBeast.event
 async def on_voice_state_update(member, before, after):
-    if before.channel is not None and after.channel is None:
-        if before.channel.id == 759201663893372928:
-            await member.guild.system_channel.send('<@' + str(member.id) + '> has been eliminated')
+    if before.channel is not None:
+        if after.channel is None:
+            if before.channel.id == 759201663893372928:
+                await member.guild.system_channel.send('<@' + str(member.id) + '> has been eliminated')
+        else:
+            if before.channel.id == 759201663893372928 and after.channel.id != 759201663893372928:
+                await member.guild.system_channel.send('<@' + str(member.id) + '> has been eliminated')
 
 
 MrBeast.run(TOKEN)
